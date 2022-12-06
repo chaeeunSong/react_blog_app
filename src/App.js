0077311b85b20d53b2c5a20514d677f6dbac6278
[Chaeeun <[chaeny0901@gmail.com]>
2022-12-06 17:00:43 +0900

    like count 기능과 정렬기능 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+    let post = 'MY BLOG';
+    let [title, titleChange] = useState(['옷 추천', '맛집 추천', '핫플레이스 추천']);
+    let [like, likePlus] = useState(0);
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="black-nav">
+            <h4>{post}</h4>
+        </div>
+
+        <button className="btn1" onClick={()=> {
+            let copy = [...title]
+            copy = copy.sort()
+            titleChange(copy)
+        }}>오름차순정렬</button>
+
+
+        <div className="list">
+            <h4>{ title[0] } <span className="like_icon" onClick={()=> {likePlus(like+1)}}>❤</span> {like}</h4>
+            <p>2월 17일 발행</p>
+        </div>
+        <div className="list">
+            <h4>{ title[1] }</h4>
+            <p>2월 17일 발행</p>
+        </div>
+        <div className="list">
+            <h4>{ title[2] }</h4>
+            <p>2월 17일 발행</p>
+        </div>
     </div>
   );
 }
